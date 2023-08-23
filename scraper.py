3cc58656421c31440ba78a79b2f015c4f7ae1776
r280822a <r280822a@gmail.com>
2023-08-23 14:15:15 +0100

    Removed while loop

--- scraper.py
+++ scraper.py
@@ -12,8 +12,6 @@ browser.find_element("xpath","/html/body/c-wiz/div/div/div/div[2]/div[1]/div[3]/
 browser.find_element("xpath","/html/body/ytd-app/div[1]/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-rich-grid-renderer/div[6]/ytd-rich-grid-row[1]/div/ytd-rich-item-renderer[1]/div/ytd-rich-grid-media/div[1]/div[1]/ytd-thumbnail/a").click()
 
 # Quit script after input
-running = True
-while(running):
-	inp = input()
-	if inp != "":
-		quit = False
+inp = input()
+if inp != "":
+	quit = False
